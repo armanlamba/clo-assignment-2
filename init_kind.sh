90b690fb144089ed1528b5eed60eee6b5567c5ca
armanlamba <armanlamba28@gmail.com>
2024-10-29 02:31:36 +0000

    changes

--- init_kind.sh
+++ init_kind.sh
@@ -13,4 +13,4 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -f ./kubectl
 
-kind create cluster --config kind.yaml 
+kind create cluster --config kind-config.yaml 
