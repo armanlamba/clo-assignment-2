0f97d5f8cc72d5cd9037b5c1ca1b3cfecea3149d
armanlamba <armanlamba28@gmail.com>
2024-10-29 10:11:45 +0000

    final commit

--- init_kind.sh
+++ init_kind.sh
@@ -12,5 +12,3 @@ rm -f ./kind
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -f ./kubectl
-
-kind create cluster --config kind-config.yaml 
